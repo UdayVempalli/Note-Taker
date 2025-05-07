f2e82d5bfd9d783e7dde58422b5d09c0871236bd
udayv <udayvempalli8@gmail.com>
2025-05-07 16:27:49 -0500

    AddNote

--- note-taker/src/App.js
+++ note-taker/src/App.js
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import { nanoid } from 'nanoid';
+import NotesList from "./components/NotesList";
+
 const App = () => {
-  return(<p>welcome to Note Taker</p>)
+  const [notes, setNotes] = useState([
+      {
+        id: nanoid(),
+        text: "1st Notes",
+        date: "05/07/2025"
+      },
+      {
+        id: nanoid(),
+        text: "1st Notes",
+        date: "05/07/2025"
+      },
+      {
+        id: nanoid(),
+        text: "2nd Notes",
+        date: "05/07/2025"
+      },
+      {
+        id: nanoid(),
+        text: "3rd Notes",
+        date: "05/07/2025"
+      },
+      {
+        id: nanoid(),
+        text: "new Notes",
+        date: "05/07/2025"
+      },
+      {
+        id: nanoid(),
+        text: "3rd Notes",
+        date: "05/07/2025"
+      },
+      {
+        id: nanoid(),
+        text: "3rd Notes",
+        date: "05/07/2025"
+      },
+]);
+  return ( 
+      <div className="container">
+          <NotesList notes={notes}/>          
+      </div>
+  );
 };
 
 export default App;
--- note-taker/src/components/NotesList.js
+++ note-taker/src/components/NotesList.js
@@ -1,8 +1,15 @@
-const NotesList = () =>{
-    return
-        <div className="notes-list">
-            <Note />
+import Note from './Note';
+import AddNote from './AddNote';
+
+
+const NotesList = ({ notes }) => {
+    return (
+        <div className='notes-list'>
+            {notes.map((note)=> (
+                <Note id={note.id} text={note.text} date={note.date} />
+            ))}
+            <AddNote />
         </div>
-    
-}
+    );
+};
 export default NotesList;
