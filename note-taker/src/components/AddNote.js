8ca172ee9263c97f30df82b6780eda590e791528
udayv <udayvempalli8@gmail.com>
2025-05-07 22:26:05 -0500

    search feature

--- note-taker/src/components/AddNote.js
+++ note-taker/src/components/AddNote.js
@@ -1,10 +1,35 @@
-const AddNote = () => {
+import { useState } from "react";
+
+
+const AddNote = ({ handleAddNote}) => {
+    const [noteText, setNoteText] = useState('');
+    const chaacterLimit = 300;
+    const handleChange = (event) => {
+        if(chaacterLimit - event.target.value.length >=0 ){
+            setNoteText(event.target.value);
+        }
+
+    };
+    
+    const handleSaveClick = () => {
+        if(noteText.trim().length > 0){
+        handleAddNote(noteText);
+        setNoteText('');
+        }
+    };
+    
     return(
     <div className="note new">
-        <textarea rows="8" cols="10" placeholder="Type to add a note..."></textarea>
+        <textarea 
+            rows="8" 
+            cols="10" 
+            placeholder="Type to add a note..."
+            value={noteText}
+            onChange={handleChange}
+        ></textarea>
         <div className="note-footer">
-            <small> 200 Remaining </small>
-            <button className="save">Save</button>
+            <small> {chaacterLimit - noteText.length}  Remaining </small>
+            <button className="save" onClick={handleSaveClick}>Save</button>
         </div>
     </div>
     );
